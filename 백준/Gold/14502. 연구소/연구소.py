5171abe08d7ad520ba4526e52871441641b58746
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-04-29 20:53:37 +0900

    [Gold V] Title: 연구소, Time: 3556 ms, Memory: 122608 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,44 +1,25 @@
 from collections import deque
+import sys
 
-result = 0
+input = sys.stdin.readline
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-n,m = map(int, input().split())
-graph = []
-temp = [[0]*m for _ in range(n)]
-
-for _ in range(n):
-  graph.append(list(map(int,input().split())))
-
-def virus(x,y):
-  queue = deque()
-  queue.append((x,y))
-
-  while queue:
-    x,y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if nx<0 or ny<0 or nx>=n or ny>=m:
-        continue
-      if temp[nx][ny]==0:
-        temp[nx][ny] = 2
-        queue.append((nx,ny))
-  
-def bfs(count):
+def create_wall(count):
   global result
-  if count == 3:
+  
+  if(count == 3):
     for i in range(n):
       for j in range(m):
         temp[i][j] = graph[i][j]
-
+        
     for i in range(n):
-      for j in range(m):
-        if temp[i][j] == 2:
-          virus(i,j)
-
+        for j in range(m):
+          
+          if(temp[i][j] == 2):
+            bfs(i,j)
+    
     score = 0
     for i in range(n):
       for j in range(m):
@@ -52,9 +33,35 @@ def bfs(count):
       if graph[i][j] == 0:
         graph[i][j] = 1
         count += 1
-        bfs(count)
+        create_wall(count)
         graph[i][j] = 0
         count -= 1
 
-bfs(0)
+
+def bfs(x,y):
+  queue = deque()
+  queue.append((x,y))
+
+  while(queue):
+    x,y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if(0<=nx<n and 0<=ny<m):
+        if(temp[nx][ny] == 0):
+          queue.append((nx,ny))
+          temp[nx][ny] = 2
+
+result = 0
+n,m = map(int, input().strip().split())
+
+graph = []
+temp = [[0]*m for _ in range(n)]    
+
+for i in range(n):
+  graph.append(list(map(int, input().strip().split())))
+
+temp2 = list()
+create_wall(0)
 print(result)
