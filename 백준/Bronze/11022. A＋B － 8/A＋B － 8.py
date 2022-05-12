d48041ccb7a6ce29f7d689f7898155ed82a17c75
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-05-12 22:42:51 +0900

    [Bronze III] Title: A+B - 8, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,6 @@
+import sys
+T=int(sys.stdin.readline().rstrip())
+for i in range(T):
+    A,B=map(int,sys.stdin.readline().split())
+    C=A+B
+    print("Case #{0}: {1} + {2} = {3}".format(i+1,A,B,C))
