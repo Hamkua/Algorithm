e753950f8d999258aecc2df8e4ea39d916f313e6
함궈 <heoja0123@naver.com>
2022-05-18 19:08:59 +0900

    Update 영역 구하기.py

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,50 +1,54 @@
-from collections import deque 
-import sys 
-input = sys.stdin.readline 
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+from collections import deque
+import sys
+input = sys.stdin.readline
 
 def bfs(x,y):
+  size = 1
+  visited[x][y] = True
   queue = deque()
   queue.append((x,y))
-  data[x][y] = 0
-  w = 1
-  while queue:
-    
-    x,y = queue.popleft()
+
+  while(queue):
+    x, y = queue.popleft()
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if 0<=nx<n and 0<=ny<m:
-        if data[nx][ny] == -1:
-          data[nx][ny] = 0
-          w += 1
+      if(0<=nx<m and 0<=ny<n):
+        if(visited[nx][ny] == False and graph[nx][ny] == 0):
+          visited[nx][ny] = True
           queue.append((nx,ny))
+          size += 1
 
-  return w
+  return size 
 
-m,n,k = map(int, input().split())
-data = [[-1]*m for _ in range(n)]
-count = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+m,n,k = map(int,input().strip().split())
+
+graph = [[0]*(n+1) for _ in range(m+1)]
+visited = [[False]*(n+1) for _ in range(m+1)]
+cnt = 0
 result = []
 
 for _ in range(k):
-  sx,sy,ex,ey = map(int,input().split())
-  for i in range(sx,ex):
-    for j in range(sy,ey):
-      data[i][j] = -2
-  
-for i in range(n):
-  for j in range(m):
-    if data[i][j] == -1:
-      
-      w = bfs(i,j)
-      result.append(w)
-      count += 1
-      
-print(count)
+  start_y, end_x, end_y, start_x = map(int, input().strip().split())
+
+  start_x = m - start_x
+  end_x = m - end_x
+  for i in range(start_x, end_x):
+    for j in range(start_y, end_y):
+      graph[i][j] = 1
+
+
+for i in range(m):
+  for j in range(n):
+    if graph[i][j] == 0 and visited[i][j] == False:
+      result.append(bfs(i,j))
+      cnt += 1
+
 result.sort()
-for i in result:
-  print(i, end=" ")
+print(cnt)
+for size in result:
+  print(size, end=" ")
