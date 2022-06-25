5f62f447f78741ddf82c96662eb4eef371aa2bf3
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-06-25 15:12:53 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 324 ms, Memory: 126872 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = sys.maxsize
+heap = []
+
+def dijkstra(start, end):
+  heapq.heappush(heap, (0, start))
+  result[start] = 0
+  while(heap):
+    cost, destination = heapq.heappop(heap)
+
+    if(result[destination] < cost):
+      continue 
+    else: 
+      for next_cost, next_destination in data[destination]:
+        new_cost = cost + next_cost
+        if result[next_destination] > new_cost:
+          result[next_destination] = new_cost
+          heapq.heappush(heap, (new_cost, next_destination))
+          path[next_destination] = []
+          for p in path[destination]:
+            path[next_destination].append(p)
+          path[next_destination].append(next_destination)
+      
+n = int(input())
+m = int(input())
+
+
+data = [[] for _ in range(n + 1)]
+path = [[] for _ in range(n + 1)]
+result = [INF] * (n + 1)
+
+for _ in range(m):
+  f, t, cost = map(int, input().strip().split())
+  data[f].append((cost, t))
+
+start, end = map(int, input().strip().split())
+path[start].append(start)
+dijkstra(start, end)
+print(result[end])
+print(len(path[end]))
+print(" ".join(map(str, path[end])))
