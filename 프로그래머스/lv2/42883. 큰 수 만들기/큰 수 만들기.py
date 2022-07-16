f2a0970af7e316b3f9b8c72fabfc81aaf3b5f0e9
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-07-16 20:11:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,20 +1,19 @@
 def solution(number, k):
-    #1. 스택 생성
-    collected = []
-
-    for (i, num) in enumerate(number):
-        #2. 만약 collected이 num보다 작은 경우 and k > 0일 경우 
-        while collected and collected[-1] < num and k > 0:
-            collected.pop()
-            k -= 1
-        
-        if k == 0:
-            collected += number[i:]
-            break
-
-        collected.append(num)
-
-    #만약 k가 0보다 크다면 collected를 뒤에서 k개 자름.
-    collected = collected[:-k] if k > 0 else collected
-    answer = "".join(collected)
+    answer = ''
+    tmp = []
+    tmp.append(number[0])
+    index = 1
+    
+    for i in range(index, len(number)):
+        while(k > 0):
+            if(len(tmp) > 0 and tmp[-1] < number[i]):
+                tmp.pop()
+                k -= 1
+            else:
+                break
+        tmp.append(number[i])
+        # print("index = {} , {}".format(i,tmp))
+    
+    tmp = tmp[:len(tmp) - k]
+    answer = "".join(tmp)
     return answer
