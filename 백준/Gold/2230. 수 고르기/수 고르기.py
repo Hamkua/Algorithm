0fd41ea8cd606a8c6cfee4a6744c21f453fe2d80
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-07-21 09:40:15 +0900

    Update 수 고르기.py

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -12,6 +12,18 @@ right = 1
 
 result = sys.maxsize
 
+# while(left < right):
+#   if(a[right] - a[left] == m):
+#     result = m
+#     break 
+#   elif(a[right] - a[left] > m):
+    
+#     result = min(result, a[right] - a[left])
+#     left += 1
+#   else:
+#     if(right + 1 < len(a)):   <- 이 조건문에도 해당되지 않는 경우 무한루프
+#       right += 1
+
 while(left < n and right < n):
   diff = a[right] - a[left]
   if(diff == m):
