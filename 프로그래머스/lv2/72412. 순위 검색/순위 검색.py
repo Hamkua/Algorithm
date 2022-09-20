8a2e505abadb4155ab0ba66ae862cb0073adc3cc
함궈 <84824124+Hamkua@users.noreply.github.com>
2022-09-20 21:10:07 +0900

    [level 2] Title: 순위 검색, Time: 485.25 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,50 +1,41 @@
-from itertools import combinations
-# from collections import defaultdict
 from bisect import bisect_left
+from itertools import combinations
 
 
 def solution(info, query):
     answer = []
-    # dictionary = defaultdict(list)
     dictionary = dict()
-
-    for info_list in info:
-        info_list = info_list.split()
-        score = int(info_list[-1])
-        info_list = info_list[:-1]
-        
+    
+    for programmer_info in info:
+        programmer_info = programmer_info.split()
+        score = int(programmer_info.pop())
         for i in range(5):
-            
-            combination_list = list(combinations(info_list, i))
+            combination_list = combinations(programmer_info, i)
             for combination in combination_list:
-                
                 key = "".join(combination)
                 if key not in dictionary:
-                    dictionary[key] = ([score])
+                    dictionary[key] = [score]
                 else:
                     dictionary[key].append(score)
-                
-                
-        
-
-        # print(dictionary)
+    
 
     for key in dictionary.keys():
         dictionary[key].sort()
-        
+
     for requirement in query:
-        requirement = [i for i in requirement.split() if i != "and" and i != "-"]
-        # print(requirement)
-        score = int(requirement[-1])
-        
-        requirement = "".join(requirement[:-1])
-        if requirement not in dictionary:
+        requirement = requirement.replace("and", "")
+        requirement = requirement.replace("-", "")
+        requirement = requirement.split()
+    
+        score = int(requirement.pop())
+    
+        key = "".join(requirement)
+    
+
+        if key not in dictionary:
             answer.append(0)
-            continue
-            
-        result = dictionary[requirement]
-        
-        idx = bisect_left(result, score)
-        answer.append(len(result) - idx)
+        else:
+            answer.append(len(dictionary[key]) - bisect_left(dictionary[key], score))
         
     return answer
+    
