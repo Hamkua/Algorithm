c54a989d42495127a751fd57f2a7bb6f6e779de3
함궈 <heoja0123@naver.com>
2022-10-05 21:58:54 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 568 ms, Memory: 19284 KB

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,54 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static List<Integer> nodes = new ArrayList<>();
-    static ArrayList<Integer> result = new ArrayList<>();
-    static int size;
 
-    public static void divide(int start, int end){
-        if(start > end){
-            return;
-        }
-        int mid = start;
-        int p = nodes.get(start);
-        result.add(0,p);
-        for(int i = start + 1; i<=end; i++){
-            if(p > nodes.get(i)){
-                mid = i;
-            }
-        }
+    static Tree root;
+    public static void main(String[] args) throws IOException{
 
-        divide(mid + 1, end);    //왜 이 둘의 순서에 따라 결과가 다른가?
-        divide(start + 1, mid);
-    }
-
-    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         while(true){
-            try {
+            try{
                 String input = br.readLine();
-                if(input.equals("") || input == null){
-                    break;
+                if(input == null || input.equals("")){
+                    throw new MyException();
                 }
-                nodes.add(Integer.parseInt(input));
-            }catch (Exception e){
+
+                Tree.insert(new Tree(Integer.parseInt(input)));
+
+
+            }catch (MyException e){
+                Tree.postOrder(root);
                 break;
             }
-
         }
 
-        size = nodes.size();
 
-        divide(0, size - 1);
+    }
 
-        for(int i = 0; i<size; i++){
-            System.out.println(result.get(i));
+    static class MyException extends Exception{ }
+
+    static class Tree{
+        int value;
+        Tree parent, left, right;
+
+        public Tree(int value){
+            this.value = value;
         }
 
-        br.close();
+        public static void postOrder(Tree node){
+            if(node != null){
+                postOrder(node.left);
+                postOrder(node.right);
+                System.out.println(node.value);
+            }
+        }
+
+        public static void insert(Tree child){
+            if(root == null){
+                root = child;
+            }else {
+                Tree parentNode = null;
+                Tree node = root;
+                while (node != null) {
+//                    System.out.print(node.value + ", ");
+                    parentNode = node;
+
+                    if (node.value > child.value) {
+
+                        node = node.left;
+
+                    }else{
+                        node = node.right;
+
+
+                    }
+                }
+//                System.out.println();
+                if(parentNode.value > child.value){
+                    parentNode.left = child;
+                }else{
+                    parentNode.right = child;
+                }
+            }
+        }
     }
 }
+
